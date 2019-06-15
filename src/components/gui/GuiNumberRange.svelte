f238f727d7019b808d171d5a216188eb1ee0954b
Chris Brown <chris@2pha.com>
2019-06-15 14:12:59 +1000

    Add Number Range component..

---
+++ src/components/gui/GuiNumberRange.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+  export let label = "";
+  export let value = 0;
+  export let min = 0.0;
+  export let max = 100;
+  export let step = 0.1;
+
+  function valueChange(e) {
+    let newval = parseFloat(e.target.value);
+    if (isNaN(newval)) {
+      return;
+    }
+    if (newval < min) {
+      newval = min;
+    } else if (newval > max) {
+      newval = max;
+    }
+    value = newval;
+    dispatch("change", {
+      key: label,
+      value: newval
+    });
+  }
+</script>
+
+<style>
+  /* css in gloobal.css */
+</style>
+
+<div class="gui-item gui-number-range">
+  <div class="gui-label">{label}</div>
+  <div class="gui-controller">
+    <input type="range" {value} {min} {max} {step} on:change={valueChange} />
+    <input type="text" {value} on:change={valueChange} />
+  </div>
+</div>
